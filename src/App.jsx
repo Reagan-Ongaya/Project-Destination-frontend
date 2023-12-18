4ed5ec73bfcdb635842565edb7effd68cf027216
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-18 22:43:13 +0300

    feature: set up navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Routes, Route} from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
+
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/home';
+import { AddDestination } from './pages/AddDestination';
+
+
 function App() {
   return (
-    <>Home</>
+    <>
+
+      {/* Navbar */}
+      <Navbar />
+        
+      <Container>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+
+          <Route  path='add-destination' element={<AddDestination />}/>
+
+        </Routes>
+      </Container>
+    </>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/AddDestination.jsx
@@ -0,0 +1,3 @@
+export const AddDestination = () => {
+    return <div>AddDestination</div>;
+};
